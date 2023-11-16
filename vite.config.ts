3a82c83564112b97efbb342c412bd1c63f579cef
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-11-16 13:11:33 +0300

    readme and vite config are changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://johnnystorm19.github.io/RA_Forms_task-3/'
 })
